cd838a1f58730e2a209a995102acaa8b1ee5728b
Martin Smith <martins@gmail.com>
2019-10-22 14:12:51 +0100

    Splits S3 methods

--- R/Partitions.R
+++ R/Partitions.R
@@ -1,12 +1,15 @@
-#' Tree2Splits
+#' As splits
 #'
 #' Converts a phylogenetic tree to an array of bipartition splits.
 #'
-#' @param tr A tree of class \code{\link[ape:read.tree]{phylo}}, with tips
-#' bearing integer labels (i.e. `tr$tip.label == 1:N`).
-#' @return Returns an object of class `Splits`. two-dimensional array.  Columns correspond to unique
-#'  bipartitions, named with the number of a node that denotes the partition.
-#'  Rows correspond to tips `1:N`.
+#' @param tr A tree of class \code{\link[ape:read.tree]{phylo}}.
+#' @param asSplits Logical specifying whether to return a `Splits` object,
+#'   or an unannotated two-dimensional array (useful where performance is
+#'   paramount).
+#' @return Returns an object of class `Splits`, or (if `asSplits = FALSE`) a
+#'  two-dimensional array of 32-bit integers, which each bit specifying whether
+#'  a tip is a member of the split.  Splits are named according to the node
+#'  that defines them.
 #'
 #' @author Martin R. Smith
 #'
@@ -14,22 +17,33 @@
 #'
 #' @importFrom ape reorder.phylo
 #' @export
-Tree2Splits <- function (tr, asSplits = TRUE) {
-  tr <- Cladewise(tr)
-  splits <- cpp_edge_to_splits(tr$edge)
+as.Splits <- function (x, ...) UseMethod('as.Splits')
+
+as.Splits.phylo <- function (x, asSplits = TRUE) {
+  x <- Cladewise(x)
+  splits <- cpp_edge_to_splits(x$edge)
   # Return:
   if (asSplits) {
-    nEdge <- dim(tr$edge)[1]
-    nTip <- length(tr$tip.label)
+    nEdge <- dim(x$edge)[1]
+    nTip <- length(x$tip.label)
     rownames(splits) <- paste0('n', (nTip + 3L):(nEdge + 1L))
     structure(splits,
               nTip = nTip,
-              tip.label = tr$tip.label,
+              tip.label = x$tip.label,
               class = 'Splits')
   }
   else {
     splits
   }
+}
+
+#' @export
+as.Splits.numeric <- function (x, tipLabels = paste0('t', seq_along(x))) {
+  if (any(x >= 2^32)) stop ("Input out of range")
+  structure(matrix(x, ncol = 1),
+            nTip = length(tipLabels),
+            tip.label = tipLabels,
+            class = 'Splits')
 }
 
 #' @exportClass Splits
@@ -48,20 +62,32 @@ print.Splits <- function (x, details = FALSE, ...) {
     #}
 
     names <- rownames(x)
-    nameLengths <- vapply(names, nchar, 0)
-    namePads <- vapply(nameLengths, function (thisLength)
-      paste0(rep(' ', max(nameLengths) - thisLength), collapse=''), character(1))
-    names <- paste0(names, namePads)
+    if (!is.null(names)) {
+
+      nameLengths <- vapply(names, nchar, 0)
+      namePads <- vapply(nameLengths, function (thisLength)
+        paste0(rep(' ', max(nameLengths) - thisLength), collapse=''), character(1))
+      names <- paste0(names, namePads)
+    } else {
+      names <- rep('', length(x))
+      nameLengths = 0L
+    }
     cat("\n ", paste0(rep(' ', max(nameLengths)), collapse = ''),
         paste0(rep(c(1:9, ' '), length.out = nTip), collapse=''))
 
     for (i in seq_len(dim(x)[1])) {
-      split <- splits[i, ]
+      split <- x[i, , drop = FALSE]
       cat("\n", names[i], ' ')
       lapply(split[-length(split)], .DecodeBinary32)
-      .DecodeBinary32(split[length(split)], stopAt = nTip %/% 32)
+      .DecodeBinary32(split[length(split)],
+                      stopAt = ifelse(nTip %% 32, nTip %% 32, 32))
     }
   }
+}
+
+#' @export
+summary.Splits <- function (x, ...) {
+  print(x, details = TRUE, ...)
 }
 
 
@@ -78,54 +104,46 @@ names.Splits <- function (x) rownames(x)
   if (appendLF) cat("\n")
 }
 
-
-#' @rdname Tree2Splits
 #' @export
-#' @keywords internal
-Tree2Bipartitions <- Tree2Splits
-
-
-#' Unique Splits
-#'
-#' Removes equivalent duplicates from a matrix of bipartitions.
-#'
-#' @param splits A logical matrix containing one named row corresponding to each
-#' terminal leaf of a tree, and each column corresponds to a bipartition split;
-#' each split divides terminals into two bipartitions; members of one
-#' are marked `TRUE` and members of the other are marked `FALSE`.
-#' @param preserveParity Logical specifying whether to preserve the `TRUE` and
-#'  `FALSE` status within each split (which takes marginally longer).  If
-#'  `FALSE`, each split will be defined such that taxa in the same partition
-#'  as the first element are marked `FALSE`, and other taxa marked `TRUE`.
-#'
-#' @return The splits element, with all duplicate splits removed.
-#'
-#' @examples
-#'   set.seed(1)
-#'   splits <- Tree2Splits(ape::rtree(6, br=NULL))
-#'   UniqueSplits(splits, preserveParity=TRUE)
-#'
-#' @author Martin R. Smith
-#' @export
-UniqueSplits <- function (splits, preserveParity = FALSE) {
-  originalParity <- splits[1, ]
-
-  # Set all splits to the same parity
-  splits[, originalParity] <- !splits[, originalParity]
-
-  # Identify duplicates
-  duplicates <- duplicated(t(splits))
-
-  # Remove duplicates
-  ret <- splits[, !duplicates, drop=FALSE]
-
-  # Return:
-  if (preserveParity) {
-    ret[, originalParity[!duplicates]] <- !ret[, originalParity[!duplicates]]
+c.Splits <- function (...) {
+  splits <- list(...)
+  nTip <- unique(vapply(splits, attr, 1, 'nTip'))
+  if (length(nTip) > 1) {
+    stop("Splits must relate to identical tips.")
+  }
+  tips <- vapply(splits, attr, character(nTip), 'tip.label')
+  if (dim(unique(tips, MARGIN = 2))[2] != 1) {
+    stop("Order of tip labels must be identical.")
   }
 
-  ret
+  x <- rbind(...)
+  structure(x,
+            nTip = nTip,
+            tip.label = tips[, 1],
+            class='Splits')
 }
+
+
+#' @export
+length.Splits <- function (x) nrow(x)
+
+#' @export
+unique.Splits <- function (x, incomparables = FALSE) {
+  at <- attributes(x)
+  nTip <- at$nTip
+  notX <- (2^32 - 1) - x
+  notX[, 1] <- (2 ^ nTip - 1) - x[, 1]
+  dupX <- x
+  useNot <- notX[, 1] < x[, 1]
+  dupX[useNot, ] <- notX[useNot, ]
+  dups <- duplicated.array(dupX, MARGIN = 1, incomparables = incomparables)
+  x <- x[!dups, , drop = FALSE]
+  at$dim <- dim(x)
+  at$dimnames <- dimnames(x)
+  attributes(x) <- at
+  x
+}
+
 
 #' Drop Single Splits
 #'
--- tests/testthat/test-Partitions.R
+++ tests/testthat/test-Partitions.R
@@ -1,12 +1,33 @@
 context("Partitions.R")
 
+test_that("Split combination", {
+  tree1 <- ape::read.tree(text='((a, b), (c, (d, e)));')
+  splits1 <- as.Splits(tree1)
+  tree2 <- ape::read.tree(text='(a, (b, (c, (d, e))));')
+  tree3 <- ape::read.tree(text='(a, (b, (c, (e, d))));')
+  tree4 <- ape::read.tree(text='(a, (b, (c, (d, f))));')
+  tree5 <- ape::read.tree(text='(a, (b, (c, (d, (e, f)))));')
+
+  expect_equal(4L, length(c(splits1, as.Splits(tree2))))
+  expect_error(c(splits1, as.Splits(tree3)))
+  expect_error(c(splits1, as.Splits(tree4)))
+  expect_error(c(splits1, as.Splits(tree5)))
+  expect_equal(c(28L, 24L, 28L, 24L),
+               as.integer(c(splits1,
+                            as.Splits(RenumberTips(tree3, letters[1:5])))[, 1]))
+  expect_equal(2L, length(unique(c(splits1, as.Splits(tree2)))))
+  expect_equal(2L, length(unique(c(splits1,
+                                   as.Splits(c(3, 7), tipLabels=letters[1:5])))))
+
+})
+
 test_that("UniqueSplits works", {
   suppressWarnings(RNGversion("3.5.0")) # Until we can require R3.6.0
   set.seed(1)
   splits6 <- Tree2Splits(ape::rtree(6, br=NULL))
   expect_equal(c('8'=FALSE, '10'=FALSE, '11'=FALSE), UniqueSplits(splits6)['t4', ])
   expect_equal(!splits6, UniqueSplits(cbind(!splits6, splits6), TRUE))
-  
+
 })
 
 test_that("Large splits don't cause memory issues", {
