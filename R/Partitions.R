d672183eeee5bb34f35dac4352f7fed0d9114f0d
Martin Smith <martins@gmail.com>
2019-10-22 15:13:45 +0100

    match and %in%

--- R/Partitions.R
+++ R/Partitions.R
@@ -17,9 +17,12 @@
 #'
 #' @importFrom ape reorder.phylo
 #' @export
-as.Splits <- function (x, ...) UseMethod('as.Splits')
+as.Splits <- function (x, tipLabels = NULL, ...) UseMethod('as.Splits')
 
-as.Splits.phylo <- function (x, asSplits = TRUE) {
+as.Splits.phylo <- function (x, tipLabels = NULL, asSplits = TRUE) {
+  if (!is.null(tipLabels)) {
+    x <- RenumberTips(x, tipLabels)
+  }
   x <- Cladewise(x)
   splits <- cpp_edge_to_splits(x$edge)
   # Return:
@@ -35,6 +38,27 @@ as.Splits.phylo <- function (x, asSplits = TRUE) {
   else {
     splits
   }
+}
+
+as.Splits.Splits <- function (x, tipLabels = NULL, ...) {
+  if (attr(x, 'tip.label') != tipLabels) {
+    stop("Order of tip labels does not match")
+  }
+}
+
+#' @export
+as.Splits.list <- function (x, tipLabels = x[[1]]$tip.label, asSplits = TRUE) {
+  if (class(x[[1]]) == 'phylo') {
+    lapply(x, as.Splits, tipLabels = tipLabels)
+  } else {
+    stop("Unsupported list type.")
+  }
+}
+
+#' @export
+as.Splits.multiPhylo <- function (x, tipLabels = x[[1]]$tip.label,
+                                  asSplits = TRUE) {
+  lapply(x, as.Splits, tipLabels = tipLabels)
 }
 
 #' @export
@@ -88,6 +112,8 @@ print.Splits <- function (x, details = FALSE, ...) {
 #' @export
 summary.Splits <- function (x, ...) {
   print(x, details = TRUE, ...)
+  cat("\n\n", paste0("Tip ", seq_len(attr(x, 'nTip')), ": ", attr(x, 'tip.label'),
+                   "\t", c(rep('', 4), '\n')))
 }
 
 
@@ -128,20 +154,43 @@ c.Splits <- function (...) {
 length.Splits <- function (x) nrow(x)
 
 #' @export
-unique.Splits <- function (x, incomparables = FALSE) {
+duplicated.Splits <- function (x, incomparables = FALSE, ...) {
+  nTip <- attr(x, 'nTip')
+  dimX <- dim(x)
+
+  dupX <- matrix(nrow = dimX[1], ncol = dimX[2])
+  dupX[, 1] <- (2 ^ nTip - 1) - x[, 1]
+  dupX[, -1] <- (2^32 - 1) - x[, -1]
+
+  useOrig <- x[, 1] < dupX[, 1]
+  dupX[useOrig, ] <- x[useOrig, ]
+
+  duplicated.array(dupX, MARGIN = 1, ...)
+}
+
+#' @export
+unique.Splits <- function (x, incomparables = FALSE, ...) {
   at <- attributes(x)
-  nTip <- at$nTip
-  notX <- (2^32 - 1) - x
-  notX[, 1] <- (2 ^ nTip - 1) - x[, 1]
-  dupX <- x
-  useNot <- notX[, 1] < x[, 1]
-  dupX[useNot, ] <- notX[useNot, ]
-  dups <- duplicated.array(dupX, MARGIN = 1, incomparables = incomparables)
+  dups <- duplicated(x, incomparables, ...)
   x <- x[!dups, , drop = FALSE]
   at$dim <- dim(x)
   at$dimnames <- dimnames(x)
   attributes(x) <- at
   x
+}
+
+#' @export
+match.Splits <- function (x, table, nomatch = NA_integer_,
+                          incomparables = NULL) {
+  ret <- which(in.Splits(x, table, incomparables))
+  ret[is.na(ret)] <- nomatch
+  ret
+}
+
+#' @export
+in.Splits <- function (x, table, incomparables = NULL, ...) {
+  duplicated(c(x, table), fromLast = TRUE,
+             incomparables = incomparables)[seq_along(x), ]
 }
 
 
--- tests/testthat/test-Partitions.R
+++ tests/testthat/test-Partitions.R
@@ -4,11 +4,13 @@ test_that("Split combination", {
   tree1 <- ape::read.tree(text='((a, b), (c, (d, e)));')
   splits1 <- as.Splits(tree1)
   tree2 <- ape::read.tree(text='(a, (b, (c, (d, e))));')
+  splits12 <- c(splits1, as.Splits(tree2))
   tree3 <- ape::read.tree(text='(a, (b, (c, (e, d))));')
   tree4 <- ape::read.tree(text='(a, (b, (c, (d, f))));')
   tree5 <- ape::read.tree(text='(a, (b, (c, (d, (e, f)))));')
 
-  expect_equal(4L, length(c(splits1, as.Splits(tree2))))
+  expect_equal(4L, length(splits12))
+  expect_equal(c(FALSE, FALSE, TRUE, TRUE), as.logical(duplicated(splits12)))
   expect_error(c(splits1, as.Splits(tree3)))
   expect_error(c(splits1, as.Splits(tree4)))
   expect_error(c(splits1, as.Splits(tree5)))
@@ -19,24 +21,5 @@ test_that("Split combination", {
   expect_equal(2L, length(unique(c(splits1,
                                    as.Splits(c(3, 7), tipLabels=letters[1:5])))))
 
-})
-
-test_that("UniqueSplits works", {
-  suppressWarnings(RNGversion("3.5.0")) # Until we can require R3.6.0
-  set.seed(1)
-  splits6 <- Tree2Splits(ape::rtree(6, br=NULL))
-  expect_equal(c('8'=FALSE, '10'=FALSE, '11'=FALSE), UniqueSplits(splits6)['t4', ])
-  expect_equal(!splits6, UniqueSplits(cbind(!splits6, splits6), TRUE))
-
-})
-
-test_that("Large splits don't cause memory issues", {
-  splits5000 <- cbind(c(rep(TRUE, 2), rep(FALSE, 4998)),
-                      c(rep(FALSE, 2), rep(TRUE, 4998)),
-                      c(rep(TRUE, 2500), rep(FALSE, 2500)),
-                      c(rep(TRUE, 2500), rep(FALSE, 2500)),
-                      c(rep(TRUE, 2500), rep(FALSE, 2500)),
-                      c(rep(FALSE, 2500), rep(TRUE, 2500))
-  )
-  expect_equal(c(5000, 2), dim(UniqueSplits(splits5000)))
+  # TODO: Fully test splits with large (>32 tip) trees
 })
