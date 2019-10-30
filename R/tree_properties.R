6bf7ed19012e71d3a29fcaa839d951548fbbffc9
Martin R. Smith <martin.smith@durham.ac.uk>
2019-10-30 16:49:29 +0000

    EdgeDistances, AddTipEverywhere (#6)
    Bugs and documentation with EdgeDistances and AddTipEverywhere

--- R/tree_properties.R
+++ R/tree_properties.R
@@ -107,6 +107,20 @@ MRCA <- function(tip1, tip2, ancestors) {
 #'
 #' @template treeParam
 #'
+#' @return A symmetrical matrix listing the number of edges that must be
+#' traversed to travel from each numbered edge to each other.
+#' The two edges straddling the root of a rooted tree
+#' are counted as a single edge.  Add a 'root' tip using [`AddTip`] if the
+#' position of the root is significant.
+#'
+#' @examples
+#'
+#' tree <- BalancedTree(5)
+#' plot(tree)
+#' ape::edgelabels()
+#'
+#' EdgeDistances(tree)
+#'
 #' @family tree navigation
 #' @author Martin R. Smith
 #' @export
@@ -143,8 +157,10 @@ EdgeDistances <- function (tree) {
   }
   ret[lower.tri(ret)] <- t(ret)[lower.tri(ret)]
 
+  origOrder <- match(tree$edge[, 2], edge[[2]])
+
   # Return:
-  ret
+  ret[origOrder, origOrder]
 }
 
 #' Non-duplicate root
