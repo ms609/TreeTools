b11bce68183332c71967f1336b37c62e4455ba62
Martin Smith <martins@gmail.com>
2019-11-25 14:37:19 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,10 @@
 ## Reminders when releasing for CRAN
 release_questions <- function() {
   c(
+    "Have you really updated codemeta.json with codemetar::write_codemeta()?",
     "Have you cleared GitHub issues for this release milestone?",
     "Is the code free of #TODOs?",
     "Have you checked the Vignettes for sanity?",
-    "Have you refreshed the package meta with codemetar::write_codemeta()?",
     "Have you updated the version number in .zenodo.json, NEWS & DESCRIPTION?"
   )
 }
