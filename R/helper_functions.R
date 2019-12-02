be0cd784b05ba744a5ae02cf16442bc2cf6c1910
Martin Smith <martins@gmail.com>
2019-12-02 09:31:16 +0000

    Drop List<->Matrix

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -9,20 +9,6 @@ SampleOne <- function (x, len = length(x)) x[sample.int(len, 1L, FALSE, NULL, FA
 
 Assert <- function (statement) if (!statement) stop(deparse(statement), " is FALSE")
 
-#' Edge list to edge matrix
-#' @param edgeList tree edges in the format list(parent, child).
-#' @return edges in the format expected by \code{tree$edge},
-#'         where \code{tree} is a tree of class \code{phylo}.
-#' @keywords internal
-#' @export
-ListToMatrix <- function (edgeList) matrix(c(edgeList[[1]], edgeList[[2]]), ncol=2)
-
-#' Edge matrix to edge list
-#' @param edge edges in the matrix format used by \code{tree$edge}, where \code{tree} is a tree of class \code{phylo}
-#' @return tree edges in the format list(parent, child).
-#' @export
-MatrixToList <- function (edge) list(edge[, 1], edge[, 2])
-
 #' Add Tree to Start of List
 #'
 #' `UnshiftTree` adds a phylogenetic tree to the start of a list of trees.
