a0f51de394b6b89072375d69472487050cc991ec
Martin Smith <martins@gmail.com>
2019-12-03 10:04:56 +0000

    Test rooted tree factory

--- tests/testthat/test-tree_shape.R
+++ tests/testthat/test-tree_shape.R
@@ -1,17 +1,55 @@
 context('tree_shape.cpp')
 
-test_that('Tree shapes calculated', {
-  tree <- PectinateTree(0:3)
-  cat("\n\n")
-  tree <- Postorder(root(tree, 1, resolve.root = TRUE))
-  edge <- tree$edge
-  nTip <- NTip(tree)
-  plot(tree); nodelabels(4:6); edgelabels(0:5);
-  edge <- PostorderEdges(edge[, 1], edge[, 2], nTip = nTip)
-
-  expect_equal(0L, TreeShape(tree))
-
-
-  PectinateTest <- function (i) expect_equal(0L, TreeShape(PectinateTree(i)))
-  lapply(4:16, PectinateTest)
+test_that('Tree shapes counted', {
+  expect_equal(c(1, 1, 1, 2, 3, 6, 11, 23),
+               vapply(1:8, NRootedTreeShapes, 0L))
 })
+
+test_that('Tree shapes calculated', {
+  expect_equal(NRootedTreeShapes(8), RootedTreeShape(BalancedTree(0:7)))
+
+  expect_equal(0L, RootedTreeShape(PectinateTree(0:3)))
+  expect_equal(1L, RootedTreeShape(BalancedTree(0:3)))
+
+  expect_equal(0L, RootedTreeShape(PectinateTree(0:4)))
+  expect_equal(1L, RootedTreeShape(as.phylo(1, 5)))
+  expect_equal(2L, RootedTreeShape(BalancedTree(0:4)))
+
+  expect_equal(0L, RootedTreeShape(PectinateTree(0:5)))
+  expect_equal(1L, RootedTreeShape(as.phylo(58, 6)))
+  expect_equal(2L, RootedTreeShape(as.phylo(1, 6)))
+  expect_equal(3L, RootedTreeShape(ape::read.tree(text='((a, b), (c, (d, (e, f))));')))
+  expect_equal(4L, RootedTreeShape(ape::read.tree(text='((a, b), ((c, d), (e, f)));')))
+  expect_equal(5L, RootedTreeShape(BalancedTree(0:5)))
+
+  PectinateTest <- function (i) expect_equal(0L, RootedTreeShape(PectinateTree(i)))
+  lapply(4:16, PectinateTest)
+
+  #BalancedTest <- function (i) expect_equal(NRootedTreeShapes(i),
+  #                                          RootedTreeShape(BalancedTree(i)))
+  #lapply(2^(2:4), BalancedTest)
+})
+
+
+test_that('Tree shapes built', {
+  expect_equal(RootedTreeWithShape(0, 4), PectinateTree(rep('', 4)))
+  expect_equal(RootedTreeWithShape(1, 4), BalancedTree(rep('', 4)))
+
+
+  expect_equal(RootedTreeWithShape(0L, 5L), PectinateTree(rep('', 5)))
+  expect_equal(RootedTreeWithShape(1L, 5L), as.phylo(1, 5, rep('', 5)))
+  expect_equal(RootedTreeWithShape(2L, 5L), BalancedTree(rep('', 5)))
+
+  blank6 <- rep('', 6L)
+  expect_equal(RootedTreeWithShape(0L, 6L), PectinateTree(blank6))
+  expect_equal(RootedTreeWithShape(1L, 6L), as.phylo(58, 6, blank6))
+  expect_equal(RootedTreeWithShape(2L, 6L), as.phylo(1, 6, blank6))
+  expect_equal(RootedTreeWithShape(3L, 6L), ape::read.tree(text='((,),(,(,(,))));'))
+  expect_equal(RootedTreeWithShape(4L, 6L), ape::read.tree(text='((,),((,),(,)));'))
+  expect_equal(RootedTreeWithShape(5L, 6L), BalancedTree(blank6))
+
+  expect_equal(RootedTreeWithShape(0, 8), PectinateTree(rep('', 8)))
+  expect_equal(RootedTreeWithShape(NRootedTreeShapes(8), 8), BalancedTree(rep('', 8)))
+
+})
+
