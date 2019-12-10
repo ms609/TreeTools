d3e8bae502e6c7d2fab28948359111f7b5f65be2
Martin Smith <martins@gmail.com>
2019-12-10 15:36:24 +0000

    str::append is quicker (?).

--- src/as_newick.cpp
+++ src/as_newick.cpp
@@ -1,5 +1,5 @@
 #include <Rcpp.h>
-#include <sstream>
+#include <string>
 using namespace Rcpp;
 
 const unsigned int MAX_NEWICK_NODES = 8192;
@@ -8,19 +8,13 @@ void close_node (const unsigned int top_node,
                  const unsigned int bottom_node,
                  bool is_open[],
                  const unsigned int n_tip,
-                 std::ostringstream& ret) {
+                 std::string& ret) {
   for (unsigned int j = top_node; j > bottom_node; j--) {
     if (is_open[j - n_tip]) {
-      ret << ")";
+      ret.append(")");
       is_open[j - n_tip] = false;
     }
   }
-}
-
-std::string to_string(const int i) {
-  std::stringstream gstream;
-  gstream << i;
-  return gstream.str();
 }
 
 // Edge should be the $edge element of a `phylo` object, minus one.
@@ -33,25 +27,26 @@ CharacterVector as_newick(IntegerMatrix edge) {
   const unsigned int n_tip = edge(0, 0);
   unsigned int last_node = 0;
   bool is_open[MAX_NEWICK_NODES];
-  std::ostringstream ret;
+  std::string ret;
+  ret.reserve(edge.nrow() * 6); /* Four characters for tip + "," + "(" or ")" */
 
   for (unsigned int i = 0; i < (unsigned int) edge.nrow(); i++) {
     unsigned int this_node = edge(i, 0), this_child = edge(i, 1);
     if (this_node == last_node) {
-      ret << ",";
+      ret.append(",");
     } else if (this_node > last_node) {
-      ret << "(";
+      ret.append("(");
       is_open[this_node - n_tip] = true;
     } else {
       close_node(last_node, this_node, is_open, n_tip, ret);
-      ret << ",";
+      ret.append(",");
     }
     if (this_child < n_tip) {
-      ret << this_child;
+      ret.append(std::to_string(this_child));
     }
     last_node = this_node;
   }
   close_node(edge(edge.nrow() - 1, 0), n_tip - 1, is_open, n_tip, ret);
-  ret << ";";
-  return CharacterVector::create(ret.str());
+  ret.append(";");
+  return CharacterVector::create(ret);
 }
