a5b943c75b7cefd6bfc2ee6eca5194ec3b9fb4c6
Martin Smith <martins@gmail.com>
2019-12-16 12:14:32 +0000

    Test tidiness

--- tests/testthat/test-tree_rearrange.R
+++ tests/testthat/test-tree_rearrange.R
@@ -6,21 +6,22 @@ test_that("CollapseNodes works", {
   tree8  <- read.tree(text="(((a, (b, (c, d))), (e, f)), (g, h));")
   expect_error(CollapseNode(1:5, tree8))
   expect_error(CollapseNode(tree8, 1))
-  
-  suppressWarnings(RNGversion("3.5.0")) # Until we can require R3.6.0
-  set.seed(1)
-  
-  tree <- rtree(7)
+
+  tree <- as.phylo(123, 7)
+  tree$edge.length <- 12:1
   expect_equal(tree, CollapseNode(tree, integer(0)))
-  
+
+  exp1213 <- matrix(c(8, 8, 9, 10, 10, 9, 11, 11, 11, 11,
+                      1, 9, 10, 2, 4, 11, 3, 7, 6, 5), ncol=2)
   no1213 <- CollapseNode(tree, c(12, 13))
-  expect_equal(no1213$edge, matrix(c(8, 9, 9, 8, 10, 11, 11, 10, 10, 10, 
-                                      9, 1, 2, 10, 11, 3:7), ncol=2))
+  expect_equal(exp1213, no1213$edge)
+
   el <- tree$edge.length
-  expect_equal(no1213$edge.length, c(el[1:7], el[8] + c(c(el[9] + el[10:11]), el[12])))
-  
-  no11 <- CollapseEdge(tree, 5L)
-  expect_equal(no11$edge, matrix(c(8, 9, 9, 8, 10, 10, 10, 11, 12, 12, 11,
-                                   9, 1, 2, 10, 3, 4, 11, 12, 5:7), ncol=2))
+  expect_equal(no1213$edge.length, c(el[1:6],
+                                     el[7] + c(c(el[8] + el[9:10]), el[11]),
+                                     el[12]))
+
+  no11 <- CollapseEdge(tree, c(7, 8))
+  expect_equal(exp1213, no11$edge)
 
 })
