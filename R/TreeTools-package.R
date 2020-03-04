400222f51d0ccb04f1bb35109c5f98e2724924b3
Martin Smith <martins@gmail.com>
2020-03-04 15:22:38 +0000

    Readme

--- R/TreeTools-package.R
+++ R/TreeTools-package.R
@@ -1,4 +1,15 @@
-#TODO
+#' 'TreeTools' is an R package that provides functions for creating, modifying and
+#' analysing phylogenetic trees.
+#' It complements packages such as
+#' ['ape'](https://cran.r-project.org/package=ape),
+#' ['phangorn'](https://cran.r-project.org/package=phangorn) and
+#' ['phytools'](https://cran.r-project.org/package=phytools),
+#' aiming for efficient and robust implementations of functions, typically
+#' applied to unweighted trees (i.e. those without edge lengths).
+#'
+#' Version 0.1 is a pre-release; some minor functionality may change in version
+#' 1.0, due in mid-2020.
+#'
 #' @keywords internal
 "_PACKAGE"
 
