0215f7106fb167382e3c95e56d13770c9364f07c
Martin Smith <martins@gmail.com>
2020-05-21 16:55:21 +0100

    Code coverage

--- tests/testthat/test-tree_information.R
+++ tests/testthat/test-tree_information.R
@@ -3,8 +3,11 @@ context("tree_information.R")
 test_that("Cladistic info calculated correctly", {
   bal8 <- BalancedTree(8)
   expect_equal(1, TreesMatchingTree(bal8))
+  expect_equal(0, LnTreesMatchingTree(bal8))
   expect_equal(Log2Unrooted(8), CladisticInfo(bal8))
   expect_equal(0L, CladisticInfo(CollapseNode(bal8, 10:15)))
+  expect_equal(rep(CladisticInfo(PectinateTree(6)), 4),
+               CladisticInfo(as.phylo(1:4, 6)))
 
   tr1 <- CollapseNode(bal8, 12:15)
   expect_equal(NUnrooted(4) * NUnrooted(5),
