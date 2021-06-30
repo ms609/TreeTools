bcc5658a26eed663abdae447b8efec53c44e6fea
Martin Smith <martins@gmail.com>
2021-06-30 06:56:26 +0100

    Delete root singleton

--- tests/testthat/test-drop_tip.cpp.R
+++ tests/testthat/test-drop_tip.cpp.R
@@ -2,4 +2,6 @@ test_that("drop_tip() works", {
   expect_equal(matrix(c(6, 7, 8, 8, 7, 6, 9, 9,
                         7, 8, 1, 2, 3, 9, 4, 5), 8, 2),
                drop_tip(BalancedTree(8)$edge, 6:4))
+  expect_equal(BalancedTree(4)$edge,
+               drop_tip(BalancedTree(8)$edge, 5:8))
 })
