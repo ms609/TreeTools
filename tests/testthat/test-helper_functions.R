6350e07d624484d89a5c1bfee5de228fecb45b0e
Martin Smith <martins@gmail.com>
2021-08-05 13:15:55 +0100

    Update test-helper_functions.R

--- tests/testthat/test-helper_functions.R
+++ tests/testthat/test-helper_functions.R
@@ -1,10 +1,11 @@
 test_that("UnshiftTree() works", {
   t1 <- as.phylo(1, 8)
-  t2..9 <- setNames(as.phylo(2:9, 8), letters[2:9])
-  t1..9 <- setNames(as.phylo(1:9, 8), c('', letters[2:9]))
-  expect_equal(ignore_attr = TRUE, t1..9, UnshiftTree(t1, t2..9))
-  expect_equal(names(t1..9), names(UnshiftTree(t1, t2..9)))
-  expect_equal(ignore_attr = TRUE, t1..9, UnshiftTree(t1, lapply(t2..9, I)))
-  expect_equal(as.phylo(1:2, 8), UnshiftTree(t1, as.phylo(2, 8)),
-               ignore_attr = TRUE)
+  t2..9 <- setNames(as.phylo(2:3, 8), letters[2:3])
+  t1..9 <- setNames(as.phylo(1:3, 8), c('', letters[2:3]))
+  attr(t1..9, 'tip.label') <- NULL
+  expect_equal(t1..9, UnshiftTree(t1, t2..9))
+  expect_equal(unclass(t1..9), UnshiftTree(t1, unclass(t2..9)))
+  expectation <- as.phylo(1:2, 8)
+  attr(expectation, 'tip.label') <- NULL
+  expect_equal(expectation, UnshiftTree(t1, as.phylo(2, 8)))
 })
