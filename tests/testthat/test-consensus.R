9bbc7138b0d68fa4c8fc6920eaa3fc0995be8e15
Martin Smith <martins@gmail.com>
2021-08-12 06:45:16 +0100

    Improve tests

--- tests/testthat/test-consensus.R
+++ tests/testthat/test-consensus.R
@@ -1,9 +1,10 @@
 test_that("Consensus()", {
 
-  ApeTest <- function (tr) {
+  ApeTest <- function (tr, p = 1) {
     # plot(Consensus(tr))
     # plot(consensus(tr))
-    expect_true(all.equal(Consensus(tr), ape::consensus(tr)))
+    expect_true(all.equal(RootTree(Consensus(tr, p = p), 1),
+                          RootTree(ape::consensus(tr, p = p), 1)))
   }
 
   trees <- list(BalancedTree(8), PectinateTree(8))[c(1, 1, 1, 1, 2, 2, 2)]
@@ -12,13 +13,10 @@ test_that("Consensus()", {
   expect_error(Consensus(trees, 0.2)) # p too low
 
   ApeTest(trees)
-  expect_equal(RootTree(BalancedTree(8), 't1'),
-               RootTree(Consensus(trees, 0.5), 't1'))
+  ApeTest(trees, 0.5)
   expect_equal(Consensus(trees, 0.999), Consensus(trees, 1))
+
   ApeTest(as.phylo(0:2, 8))
-  par(mfrow=2:1, mar = rep(0, 4))
-  plot(Consensus(as.phylo(0:2, 8))); plot(consensus(as.phylo(0:2, 8))); legend('bottomleft', 'APE')
   ApeTest(as.phylo(0:250, 8))
   ApeTest(as.phylo(0:250, 80))
-  ApeTest(as.phylo(0:250, 800))
 })
