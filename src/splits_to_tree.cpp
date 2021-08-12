45f2b9f2255edae3a2a4bd15157e398d9993bea4
Martin Smith <martins@gmail.com>
2021-08-12 11:22:32 +0100

    bin_tip

--- src/splits_to_tree.cpp
+++ src/splits_to_tree.cpp
@@ -67,8 +67,8 @@ IntegerMatrix splits_to_edge(const RawMatrix splits, const IntegerVector nTip) {
       const splitbit chunk = x.state[split][bin];
       for (int16 bin_tip = SL_BIN_SIZE; bin_tip--; ) {
         const int16 tip = bin_tip + (bin * SL_BIN_SIZE);
-        if (chunk & powers_of_two[tip]) {
           // Rcout << "  Split " << split << " contains " << tip << ".\n";
+        if (chunk & powers_of_two[bin_tip]) {
           insert_ancestor(tip, next_node, parent);
         }
       }
