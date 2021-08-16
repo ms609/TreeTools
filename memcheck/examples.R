7897855a6950dcea0b23d292c60e447c7c878906
Martin Smith <martins@gmail.com>
2021-08-16 09:39:41 +0100

    hone workflows

--- memcheck/examples.R
+++ memcheck/examples.R
@@ -1,5 +1,3 @@
 # Code to be run with
 #   R -d "valgrind --tool=memcheck --leak-check=full --error-exitcode=1" --vanilla < tests/thisfile.R
-# First build and install the package.
-library('TreeTools')
 devtools::run_examples()
