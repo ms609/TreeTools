19a12036e8f61617262e15500fa2ebc7b1924c53
Martin Smith <martins@gmail.com>
2021-08-16 08:35:32 +0100

    --error-exitcode=1

--- memcheck/examples.R
+++ memcheck/examples.R
@@ -1,5 +1,5 @@
 # Code to be run with
-#   R -d "valgrind --tool=memcheck --leak-check=full" --vanilla < tests/thisfile.R
+#   R -d "valgrind --tool=memcheck --leak-check=full --error-exitcode=1" --vanilla < tests/thisfile.R
 # First build and install the package.
 library('TreeTools')
 devtools::run_examples()
