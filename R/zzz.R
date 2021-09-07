ad4c83dfbef8bc21c601f2a570a07f23b56612fd
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-07 09:07:56 +0100

    Expand description text

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,8 @@
 release_questions <- function() {
   c(
     "Is the code free of #TODOs?",
-    "Have you checked the Vignettes for sanity?"
+    "Have you checked the Vignettes for sanity?",
+    "Have you updated pagination of Asher2020 (Syst Biol)?"
   )
 }
 
