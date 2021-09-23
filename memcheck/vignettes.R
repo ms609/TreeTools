5570365dd787a78494f97afe3d097ff35cff8ccd
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-23 11:59:34 +0100

    ASan checks

--- memcheck/vignettes.R
+++ memcheck/vignettes.R
@@ -1,5 +1,4 @@
 # Code to be run with
 #   R -d "valgrind --tool=memcheck --leak-check=full --error-exitcode=1" --vanilla < tests/thisfile.R
 # First build and install the package.
-library('TreeTools')
-devtools::build_vignettes()
+devtools::build_vignettes(install = FALSE)
