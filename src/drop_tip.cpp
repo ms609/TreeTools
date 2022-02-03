6ac2058569b24d7a75986010c09260bcc01a4c68
Martin R. Smith <pjjg18@durham.ac.uk>
2022-02-03 07:20:52 +0000

    Better semantic naming of variables

--- src/drop_tip.cpp
+++ src/drop_tip.cpp
@@ -22,45 +22,47 @@ IntegerMatrix drop_tip (const IntegerMatrix edge, const IntegerVector drop) {
   IntegerVector new_child = edge(_, 1);
   auto
     parent_of = std::make_unique<int[]>(all_nodes),
-    as_parent = std::make_unique<int[]>(all_nodes),
-    child_on = std::make_unique<int[]>(all_nodes),
-    new_no = std::make_unique<int[]>(all_nodes)
+    n_children = std::make_unique<int[]>(all_nodes),
+    edge_above = std::make_unique<int[]>(all_nodes)
   ;
   for (int i = start_edge; i--; ) {
-    const int parent = edge(i, 0), child = new_child[i];
+    const int
+      parent = edge(i, 0),
+      child = new_child[i]
+    ;
     parent_of[child] = parent;
-    ++as_parent[parent];
-    child_on[child] = i;
+    ++n_children[parent];
+    edge_above[child] = i;
   }
 
   for (int i = drop.length(); i--; ) {
     const int tip = drop(i);
     dropped_node[tip] = true;
-    dropped_edge[child_on[tip]] = true;
+    dropped_edge[edge_above[tip]] = true;
 
     const int node_parent = parent_of[tip];
 #ifdef MSDEBUG
-    Rcout << " Dropping tip " << tip << " on edge 1+" << child_on[tip] << ".\n";
+    Rcout << " Dropping tip " << tip << " on edge 1+" << edge_above[tip] << ".\n";
     Rcout << "  Parent of tip = " << node_parent << ", instance "
-          << as_parent[node_parent] <<"\n";
+          << n_children[node_parent] <<"\n";
 #endif
     parent_of[tip] = 0;
-    if (--as_parent[node_parent] == 1) {
+    if (--n_children[node_parent] == 1) {
       int child = 0;
       do {++child;} while (parent_of[child] != node_parent);
-      dropped_edge[child_on[child]] = true;
+      dropped_edge[edge_above[child]] = true;
       const int
         lost_node = parent_of[child],
-        child_destination = child_on[lost_node]
+        child_destination = edge_above[lost_node]
       ;
 #ifdef MSDEBUG
       Rcout << " Lost node " << lost_node << "; moving child " << child
             << " to edge 1+"
-            << child_on[lost_node] << " and setting parent to "
+            << edge_above[lost_node] << " and setting parent to "
             << parent_of[lost_node] << ".\n";
 #endif
       dropped_node[lost_node] = true;
-      child_on[child] = child_destination;
+      edge_above[child] = child_destination;
       new_child[child_destination] = child;
       parent_of[child] = parent_of[lost_node];
       parent_of[lost_node] = 0;
@@ -69,6 +71,7 @@ IntegerMatrix drop_tip (const IntegerMatrix edge, const IntegerVector drop) {
 
 
   int dropped = 0;
+  auto new_no = std::make_unique<int[]>(all_nodes);
   for (int i = 1; i != all_nodes; i++) {
     if (dropped_node[i]) {
       ++dropped;
