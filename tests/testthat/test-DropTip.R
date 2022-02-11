a661232d2de20f4fd9226148885505376e77c87d
Martin R. Smith <pjjg18@durham.ac.uk>
2022-02-11 09:04:15 +0000

    pinpoint error

--- tests/testthat/test-DropTip.R
+++ tests/testthat/test-DropTip.R
@@ -8,6 +8,7 @@ nasty <- structure(list(edge = structure(
 
 test_that("keep_tip() works", {
   expect_true(TRUE) # TODO delete
+  skip_if(TRUE)
   expect_error(keep_tip(BalancedTree(8)$edge[, c(1, 2, 1)], !tabulate(6:4, 8)))
   expect_error(keep_tip(BalancedTree(8)$edge[, 1, drop = FALSE],
                         !tabulate(6:4, 8)))
