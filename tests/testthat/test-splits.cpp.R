7caf8febd573b17f26dcb5b02da2ca9d781dd30c
Martin R. Smith <pjjg18@durham.ac.uk>
2022-02-11 19:01:52 +0000

    Update test-splits.cpp.R

--- tests/testthat/test-splits.cpp.R
+++ tests/testthat/test-splits.cpp.R
@@ -2,5 +2,6 @@ test_that("Bad data results in error", {
   expect_error(cpp_edge_to_splits(matrix(1, 3, 3), 1:3, 3))
   expect_error(cpp_edge_to_splits(matrix(1, 10, 2), 1:10, 0))
   expect_error(cpp_edge_to_splits(matrix(1, 10, 2), 1:10, -10))
+  expect_error(cpp_edge_to_splits(matrix(1, 10, 2), 1:9, 5))
   expect_error(cpp_edge_to_splits(matrix(1, 2, 2), 1:2, 6)) # Impossible anyway...
 })
