a9952ff1421350199fe324f4b644e4487000e162
Martin R. Smith <pjjg18@durham.ac.uk>
2022-02-11 19:20:52 +0000

    Skip on ASAN
    Fasel +ve?

--- tests/testthat/test-splits.cpp.R
+++ tests/testthat/test-splits.cpp.R
@@ -1,4 +1,5 @@
 test_that("Bad data results in error", {
+  skip_if(Sys.getenv("USING_ASAN") != "")
   expect_error(cpp_edge_to_splits(matrix(1, 3, 3), 1:3, 3))
   expect_error(cpp_edge_to_splits(matrix(1, 10, 2), 1:10, 0))
   expect_error(cpp_edge_to_splits(matrix(1, 10, 2), 1:10, -10))
