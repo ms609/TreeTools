eb13d2eaa2693bdb3ea7656629dcc75e3e0baa4a
Martin R. Smith <pjjg18@durham.ac.uk>
2022-02-14 10:35:18 +0000

    Input validation

--- R/tree_generation.R
+++ R/tree_generation.R
@@ -106,10 +106,10 @@ RandomTree <- function(tips, root = FALSE, nodes) {
 #' @importFrom stats runif
 #' @export
 .RandomParent <- function(n, seed = sample.int(2147483647L, 1L)) {
-  if (n[0] < 2) {
+  if (!is.numeric(n) || is.na(n) || n[1] < 2) {
     stop("nTip must be > 1");
   }
-  random_parent(n, seed)
+  random_parent(as.integer(n), as.integer(seed))
 }
 
 #' @rdname GenerateTree
