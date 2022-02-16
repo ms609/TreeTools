b675fc647e4beb72e3850f08a91aff513823f192
Martin R. Smith <pjjg18@durham.ac.uk>
2022-02-16 21:52:18 +0000

    Simplify & fmt

--- tests/testthat/test-TotalCopheneticIndex.R
+++ tests/testthat/test-TotalCopheneticIndex.R
@@ -1,23 +1,23 @@
 test_that("Trees from Mir et al. 2013 are scored correctly", {
   Tree <- function(text) ape::read.tree(text = text)
-  expect_identical(0L,  TotalCopheneticIndex(Tree('(1,2,3,4,5);')))
-  expect_identical(1L,  TotalCopheneticIndex(Tree('((1,2),3,4,5);')))
-  expect_identical(2L,  TotalCopheneticIndex(Tree('((1,2),(3,4),5);')))
-  expect_identical(3L,  TotalCopheneticIndex(Tree('((1,2,3),4,5);')))
-  expect_identical(4L,  TotalCopheneticIndex(Tree('(((1,2),3),4,5);')))
-  expect_identical(4L,  TotalCopheneticIndex(Tree('((1,2,3),(4,5));')))
-  expect_identical(5L,  TotalCopheneticIndex(Tree('(((1,2),3),(4,5));')))
-  expect_identical(6L,  TotalCopheneticIndex(Tree('((1,2,3,4),5);')))
-  expect_identical(7L,  TotalCopheneticIndex(Tree('(((1,2),3,4),5);')))
-  expect_identical(8L,  TotalCopheneticIndex(Tree('(((1,2),(3,4)),5);')))
-  expect_identical(9L,  TotalCopheneticIndex(Tree('(((1,2,3),4),5);')))
+  expect_identical(0L,  TotalCopheneticIndex(Tree("(1,2,3,4,5);")))
+  expect_identical(1L,  TotalCopheneticIndex(Tree("((1,2),3,4,5);")))
+  expect_identical(2L,  TotalCopheneticIndex(Tree("((1,2),(3,4),5);")))
+  expect_identical(3L,  TotalCopheneticIndex(Tree("((1,2,3),4,5);")))
+  expect_identical(4L,  TotalCopheneticIndex(Tree("(((1,2),3),4,5);")))
+  expect_identical(4L,  TotalCopheneticIndex(Tree("((1,2,3),(4,5));")))
+  expect_identical(5L,  TotalCopheneticIndex(Tree("(((1,2),3),(4,5));")))
+  expect_identical(6L,  TotalCopheneticIndex(Tree("((1,2,3,4),5);")))
+  expect_identical(7L,  TotalCopheneticIndex(Tree("(((1,2),3,4),5);")))
+  expect_identical(8L,  TotalCopheneticIndex(Tree("(((1,2),(3,4)),5);")))
+  expect_identical(9L,  TotalCopheneticIndex(Tree("(((1,2,3),4),5);")))
   expect_identical(10L, TotalCopheneticIndex(PectinateTree(5)))
   expect_identical(c(10L, 8L, 10L), TotalCopheneticIndex(as.phylo(0:2, 5)))
 
   expect_equal(TotalCopheneticIndex(BalancedTree(13)),
-               TCIContext(13)[1, 'minimum'])
+               TCIContext(13)[1, "minimum"])
   expect_equal(TotalCopheneticIndex(PectinateTree(13)),
-               TCIContext(13)[1, 'maximum'])
+               TCIContext(13)[1, "maximum"])
 
   nasty <- structure(list(edge = structure(
     c(9, 12, 10, 13, 11, 10, 11, 13, 10, 13, 12, 9,
@@ -25,7 +25,7 @@ test_that("Trees from Mir et al. 2013 are scored correctly", {
     .Dim = c(12, 2)),
     Nnode = 5L,
     tip.label = letters[1:8]),
-    class = 'phylo')
+    class = "phylo")
   expect_equal(28L, TotalCopheneticIndex(nasty))
 
   expect_equal(TCIContext(BalancedTree(5)), TCIContext(5L))
