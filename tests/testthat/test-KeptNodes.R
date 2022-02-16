c0e256eedfea8c0bbfb0240fe6f5363c5b2a1529
Martin R. Smith <pjjg18@durham.ac.uk>
2022-02-16 13:32:12 +0000

    Input checking

--- tests/testthat/test-KeptNodes.R
+++ tests/testthat/test-KeptNodes.R
@@ -4,6 +4,7 @@ test_that("KeptVerts() works", {
   bal12 <- BalancedTree(12)
   Y <- TRUE
   N <- FALSE
+  expect_error(KeptVerts(Postorder(bal12), Keepers(1:10, 12)), "in preorder")
   expect_equal(KeptVerts(bal12, 1:12 %in% 1:9),
                c(1:12 %in% 1:9, rep(Y, 6), N, Y, Y, N, N))
   expect_equal(KeptVerts(bal12, 1:12 %in% 7:12),
@@ -12,6 +13,7 @@ test_that("KeptVerts() works", {
                c(1:12 %in% 4:6, rep(N, 4), rep(Y, 2), rep(N, 5)))
   expect_equal(KeptVerts(bal12, 1:12 %in% c(1, 2, 6, 12)),
                c(1:12 %in% c(1, 2, 6, 12), Y, Y, N, Y, N, N, N, N, N, N, N))
+  expect_equal(KeptVerts(bal12, rep(Y, 12)), rep(TRUE, 23))
   
   pec9 <- UnrootTree(PectinateTree(9))
   kept <- c(1, 3, 8, 9)
