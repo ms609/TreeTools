28e20d79c9fc7f3da0ff0e2f3bd65552faec09c3
Martin R. Smith <pjjg18@durham.ac.uk>
2022-03-16 08:27:16 +0000

    Export some C++ functions

--- R/RcppExports-manual.R
+++ R/RcppExports-manual.R
@@ -14,6 +14,32 @@ root_binary <- function(edge, outgroup) {
   .Call(`_TreeTools_root_binary`, edge, outgroup)
 }
 
+#' C++ wrapper for root_on_node
+#' 
+#' Wrapper for C++ function `root_on_node()`.
+#' No attempt is made to validate input; specifying invalid input may crash
+#' R.
+#' Behaviour is subject to change without notice.
+#' 
+#' @param phy Tree of class `phylo`. root node must == n_tip + 1.
+#' @param outgroup Integer specifying new root node.
+#' @template MRS
+#' @export
 root_on_node <- function(phy, outgroup) {
   .Call(`_TreeTools_root_on_node`, phy, outgroup)
+}
+
+#' C++ wrapper for keep_tip
+#' 
+#' Wrapper for C++ function `keep_tip()`.
+#' No attempt is made to validate input; specifying invalid input may crash
+#' R.
+#' Behaviour is subject to change without notice.
+#' 
+#' @param edge Tree of class `phylo`, in (loose) preorder.
+#' @param keep Logical vector specifying whether to keep each leaf.
+#' @template MRS
+#' @export
+keep_tip <- function(edge, keep) {
+  .Call(`_TreeTools_keep_tip`, edge, keep)
 }
