9c5298630c7257fdcc6dad32998633bd231d5ac5
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-11-25 10:45:08 +0000

    Don't overwrite or t()

--- R/as.matrix.R
+++ R/as.matrix.R
@@ -1,16 +1,13 @@
 #' @export
 as.matrix.phylo <- function(x, ...) {
   sp <- as.Splits(x)
-  t(as.matrix(sp) * 1)
+  as.logical(sp) * 1
 }
 
 #' @export
 as.logical.phylo <- function(x, ...) {
-  t(as.logical(as.Splits(x)))
+  as.logical(as.Splits(x))
 }
-
-#' @export
-as.matrix.Splits <- as.logical.Splits
 
 #' @export
 as.matrix.phyDat <- function(x, ambigNA = FALSE, inappNA = ambigNA, ...) {
--- tests/testthat/test-as.matrix.R
+++ tests/testthat/test-as.matrix.R
@@ -1,8 +1,7 @@
 test_that("as.matrix(phylo)", {
   bal8 <- BalancedTree(8)
   sp8 <- as.Splits(bal8)
-  expect_equal(as.matrix(bal8), t(1 * as.logical(sp8)))
+  expect_equal(as.matrix(bal8), 1 * as.logical(sp8))
   expect_equal(TipLabels(as.matrix(bal8)), TipLabels(bal8))
-  expect_equal(as.logical(bal8), t(as.logical(sp8)))
-  expect_equal(as.matrix(sp8), as.logical(sp8))
-}
+  expect_equal(as.logical(bal8), as.logical(sp8))
+})
