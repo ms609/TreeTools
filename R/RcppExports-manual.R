02205489037cb1c34ed3709adb78682654efeb0b
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-02-23 15:31:12 +0000

    Export root_on_node()
    Useful for SPR.Dist

--- R/RcppExports-manual.R
+++ R/RcppExports-manual.R
@@ -18,6 +18,20 @@ root_binary <- function(edge, outgroup) {
   .Call(`_TreeTools_root_binary`, edge, outgroup)
 }
 
+#' Wrapper for internal C function `root_on_node()`
+#' 
+#' Direct entry point to `root_on_node()`; recommended for expert use only.
+#' `RootTree()` checks that input is properly formatted and is recommended
+#' for general use.
+#' @param phy Minimally, a named list with entries `edge` and `Nnode`, in the
+#' format of equivalent entries in a tree of class `phylo`. `edge.length` will
+#' also be considered if supplied.
+#' @param outgroup Numeric specifying index of leaf to use as outgroup.
+#' @returns `root_on_node()` returns `phy` rooted on the speficied leaf.
+#' 
+#' @template MRS
+#' @export
+#' @keywords internal
 root_on_node <- function(phy, outgroup) {
   .Call(`_TreeTools_root_on_node`, phy, outgroup)
 }
