a95d2c8a5614f93cd86b14beadd0701c12f9e4ad
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-03-06 07:22:08 +0000

    Update test-inf-cons.R

--- tests/testthat/test-inf-cons.R
+++ tests/testthat/test-inf-cons.R
@@ -1,18 +1,18 @@
 test_that("Inf cons runs", {
   trees <- as.phylo(0:5, 8)
-  if (interactive()) {
-    plot(trees[[1]])
-    nodelabels(c(0, 0:5))
-  }
+
   counts <- count_splits(trees)
-  expect_equal(sum(counts$count), NSplits(trees[[1]]) * length(trees))
-  expect_equal(ncol(counts$splits), NTip(trees[[1]]))
-  splits <- as.Splits(counts$splits)
-  expect_false(any(duplicated(splits)))
+  all_splits <- as.Splits(counts$splits)
+  dup <- duplicated(all_splits)
   
-  skip_if(TRUE)
-  cons <- Consensus(trees)
-  expect_equal(mode(cons), "list")
-  expect_equal(length(cons), 4)
-  dput(cons)
+  expect_equal(sum(counts$count[!dup]), NSplits(trees[[1]]) * length(trees))
+  expect_equal(ncol(counts$splits), NTip(trees[[1]]))
+  
+  splits <- all_splits[[!dup]]
+  nTrees <- length(trees)
+  maj_splits <- splits[[counts[["count"]][!dup] > nTrees / 2]]
+  cons_splits <- as.Splits(Consensus(trees, p = 0.5, inf = FALSE))
+  
+  expect_equal(length(maj_splits), length(cons_splits))
+  expect_true(all(maj_splits %in% cons_splits))
 })
