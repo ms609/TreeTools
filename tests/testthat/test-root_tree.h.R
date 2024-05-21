6415ab8ad9ca973c3714f5ae4ab4c8add209c971
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-05-21 16:53:05 +0100

    Errors with weighted trees

--- tests/testthat/test-root_tree.h.R
+++ tests/testthat/test-root_tree.h.R
@@ -61,3 +61,45 @@ test_that("Polytomous trees are rootable", {
     text = "(((2, 4, 5, 7, 9, 10, 12, 13), (1, 14)), (6, (8, 11)), 3);"))
   Test(t2, 1)
 })
+
+test_that("Rooted trees report preorder accurately", {
+  set.seed(1)
+  nTips <- 8
+  edge <- do.call(cbind,
+                  RenumberEdges(.RandomParent(nTips),
+                                seq_len(nTips + nTips - 2L)))
+  
+  expect_preorder <- function(x) {
+    expect_equal(x, Preorder(`attr<-`(x, "order", "unknown")))
+  }
+  
+  # Check that we are in preorder
+  expect_preorder(edge)
+  
+  expect_preorder(root_binary(edge, 2))
+  expect_preorder(root_binary(edge, 6))
+  
+  tree <- structure(list(edge = edge,
+                         Nnode = nTips - 1L,
+                         tip.label = TipLabels(nTips)),
+                    order = "preorder",
+                    class = "phylo")
+  
+  rootNode <- nTips + 1L
+  expect_preorder(root_on_node(tree, rootNode))
+  deepNode <- 2 * nTips - 2
+  expect_preorder(root_on_node(tree, deepNode))
+  
+  weighted <- tree
+  weighted[["edge.length"]] <- seq_len(dim(edge)[[1]])
+  # Currently failing:
+  expect_equal(sort(root_on_node(weighted, rootNode)[["edge.length"]]),
+                    weighted[["edge.length"]])
+  expect_preorder(root_on_node(weighted, rootNode))
+  expect_preorder(root_on_node(weighted, deepNode))
+  # Motivation: 
+  # 
+  # set.seed(0)
+  # trueTree <- TreeTools::RandomTree(20, root = TRUE)
+  # TreeSearch::TBR(trueTree)$edge # explodes
+})
