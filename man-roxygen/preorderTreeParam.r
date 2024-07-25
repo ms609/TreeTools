4b2a18d58f8915d29b1d42aa51251185426c2964
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-07-25 13:06:26 +0100

    lwd

--- man-roxygen/preorderTreeParam.r
+++ man-roxygen/preorderTreeParam.r
@@ -1,3 +1,3 @@
-#' @param tree A tree of class  [`phylo`][ape::read.tree], with internal numbering
-#' in cladewise order (use \code{\link{Preorder}(tree)} or (slower)
+#' @param tree A tree of class [`phylo`][ape::read.tree], with internal
+#' numbering in cladewise order (use \code{\link{Preorder}(tree)} or (slower)
 #' \code{\link{Cladewise}(tree)}) .
