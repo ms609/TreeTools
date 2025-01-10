afa8f7e178f8b9bc3e3759b9af71993531640e0e
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-01-10 13:57:46 +0000

    Stringstream for resilience

--- inst/include/TreeTools/assert.h
+++ inst/include/TreeTools/assert.h
@@ -1,9 +1,12 @@
 #ifndef TreeTools_assert_
 #define TreeTools_assert_
+#include <sstream>
 
 #ifdef DEBUG
 #define ASSERT(x) if (!(x)) {                                  \
-Rcpp::stop("Assertion failed: " #x);                           \
+std::ostringstream oss;                                        \
+oss << "Assertion failed: " << #x;                             \
+Rcpp::stop(oss.str());                                         \
 }
 #else
 #define ASSERT(x) ((void)0)
