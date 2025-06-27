e73114852cd3c8d99ec383a9940cdef44182b0f5
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-06-27 11:45:48 +0100

    typo

--- tests/testthat/test-MatchStrings.R
+++ tests/testthat/test-MatchStrings.R
@@ -1,5 +1,5 @@
 test_that("MatchStrings()", {
-  list1 <- "maus"
+  list1 <- "Mouse"
   list2 <-c("cat", "mouse", "dog")
   expect_error(MatchStrings("maus", list1, stop),
                "Could not find 'maus' in list1.  Did you mean 'Mouse'?")
