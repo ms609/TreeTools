5924025614b084773ce7c1e383afb8eeccccf002
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2025-07-16 14:03:12 +0100

    syntax format

--- R/data.R
+++ R/data.R
@@ -12,10 +12,10 @@
 #' }
 #'
 #' @examples
-#' data("brewer", package="TreeTools")
-#' plot(0, type="n", xlim=c(1, 12), ylim=c(12, 1),
+#' data("brewer", package = "TreeTools")
+#' plot(0, type = "n", xlim = c(1, 12), ylim = c(12, 1),
 #'      xlab = "Colour", ylab="Palette")
-#' for (i in seq_along(brewer)) text(seq_len(i), i, col=brewer[[i]])
+#' for (i in seq_along(brewer)) text(seq_len(i), i, col = brewer[[i]])
 #'
 #' @keywords datasets
 #' @name brewer
