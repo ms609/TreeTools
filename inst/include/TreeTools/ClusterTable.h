d1482a2cecc5ca837c315d4080f53e0e0bf57ea2
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-07-28 14:03:28 +0100

    Xswitch notes

--- inst/include/TreeTools/ClusterTable.h
+++ inst/include/TreeTools/ClusterTable.h
@@ -56,6 +56,9 @@ namespace TreeTools {
       T,
       visited_nth
     ;
+    // Using bitset; can obtain a ~1% speedup using vector of ULLs
+    // Retaining slower code as easier to read.
+    // See branch ct-xswitch for implementation
     std::bitset<CT_MAX_LEAVES + 1> Xswitch;
     Rcpp::IntegerMatrix Xarr;
 
@@ -326,7 +329,6 @@ namespace TreeTools {
     // BUILD Cluster table
     X_ROWS = n_leaves;
     Xarr = Rcpp::IntegerMatrix(X_COLS, X_ROWS);
-    // Xswitch = std::bitset<DAY_MAX_LEAVES>;
 
     // This procedure constructs in X descriptions of the clusters in a
     // rooted tree described by the postorder sequence T with weights,
