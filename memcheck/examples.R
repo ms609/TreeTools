c8bc6d1ab4072d2d300b49ba5e565489949deeac
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-07-28 15:19:37 +0100

    Run in same process
    Work around asan failure

--- memcheck/examples.R
+++ memcheck/examples.R
@@ -1,3 +1,14 @@
 # Code to be run with
 #   R -d "valgrind --tool=memcheck --leak-check=full --error-exitcode=1" --vanilla < memcheck/thisfile.R
-devtools::run_examples()
+topics <- tools::Rd_db("TreeTools")
+
+cat("Running examples for", length(topics), "topics\n")
+
+for (topic in names(topics)) {
+  cat("\n>>> Example:", topic, "\n")
+  tryCatch({
+    tools::runExample("TreeTools", topic = topic)
+  }, error = function(e) {
+    cat("Error in topic:", topic, "\n", conditionMessage(e), "\n")
+  })
+}
