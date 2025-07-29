6ccd96d6f00632a12088fd265f03dfe82428fb7e
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-07-29 09:27:14 +0100

    Test with complex trees

--- tests/testthat/test-ClusterTable.R
+++ tests/testthat/test-ClusterTable.R
@@ -19,13 +19,13 @@ test_that("ClusterTable class behaves", {
   expect_equal(
     capture.output(summary(ct)),
     c("ClusterTable on 6 leaves:",
-          " 123456",
-          " .**...",
-          " ***...",
-          " ****..",
-          " *****.",
-          " ******",
-          " 1: t6  2: t5  3: t4  4: t3  5: t2  6: t1 ")
+      " 123456",
+      " .**...",
+      " ***...",
+      " ****..",
+      " *****.",
+      " ******",
+      " 1: t6  2: t5  3: t4  4: t3  5: t2  6: t1 ")
   )
   
   ClusterSummary <- function(...) capture.output(summary(as.ClusterTable(...)))
@@ -82,4 +82,56 @@ test_that("Attributes are correct", {
   expect_equal(7:8, NTip(list(t7, t8)))
   
   #TODO test TipLabels, SplitsInBalancedTree
+})
+
+test_that("ClusterTable with multiple trees", {
+  tree1 <- ape::read.tree(text = "(A, (B, (C, (D, E))));");
+  tree2 <- ape::read.tree(text = "(E, (B, (D, (C, A))));");
+  ct <- as.ClusterTable(c(tree1, tree2))
+  expect_equal(
+    capture.output(print(ct)),
+    capture.output(print(list(as.ClusterTable(tree1),
+                              as.ClusterTable(tree2, TipLabels(tree1)))))
+  )
+  expect_equal(
+    capture.output(summary(ct)),
+    capture.output(summary(list(as.ClusterTable(tree1),
+                                as.ClusterTable(tree2, TipLabels(tree1)))))
+  )
+  
+  trees <- ape::read.tree(text = 
+                            "(A, (B, (C, (D, E))));(E, (B, (D, (C, A))));");
+  expect_equal(
+    capture.output(print(as.ClusterTable(trees))),
+    capture.output(print(list(as.ClusterTable(trees[[1]]),
+                              as.ClusterTable(trees[[2]], TipLabels(tree1)))))
+  )
+})
+
+test_that("ClusterTable with complex trees", {
+  skip_if_not_installed("TreeDist")
+  library("TreeDist")
+  
+  # Test exposes failures in C++ - constexpr not playing nicely with Rcpp
+  # Specifically if replacing
+  # const int16 L_COL = int16(0);
+  # const int16 R_COL = int16(1);
+  # const int16 X_COLS = int16(2);
+  # hard-coding, using enum all fails.
+  tr1 <- structure(list(
+    edge = structure(c(8L, 8L, 9L, 10L, 10L, 9L, 11L, 11L, 8L, 12L, 12L,
+                       1L, 9L, 10L, 2L, 3L, 11L, 4L, 5L, 12L, 6L, 7L),
+                     dim = c(11L, 2L)),
+    Nnode = 5L, tip.label = c("t1", "t2", "t3", "t4", "t5", "t6", "t7")),
+    class = "phylo", order = "preorder")
+  tr2 <- structure(list(
+    edge = structure(c(8L, 9L, 10L, 10L, 9L, 11L, 11L, 8L, 12L, 12L, 8L,
+                       9L, 10L, 1L, 2L, 11L, 3L, 4L, 12L, 5L, 6L, 7L),
+                     dim = c(11L, 2L)),
+    Nnode = 5L, tip.label = c("t1", "t2", "t3", "t4", "t5", "t6", "t7")),
+    class = "phylo", order = "preorder")
+  t4 <- list(a = tr1, b = tr2, c = tr1, d = tr2)
+  r4 <- RootTree(t4, 1)
+  
+  expect_equal(as.numeric(RobinsonFoulds(r4)), c(8, 0, 8, 8, 0, 8))
 })
