7e251e35746098405474477b1528c0ea20926e89
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-07-29 09:09:54 +0100

    test SplitConflicts

--- tests/testthat/test-SplitConsistent.R
+++ tests/testthat/test-SplitConsistent.R
@@ -13,6 +13,7 @@ test_that("Split contradiction is calculated", {
   expect_equal(SplitConsistent(pec12[[2]], bal12),
                SplitConsistent(pec12[[2]], list(bal12)))
   expect_error(SplitConsistent(pec12[[2]], "bal12"), "must be a Splits object")
+  expect_error(SplitConflicts(pec12[[2]], "bal12"), "must be a Splits object")
   expect_error(sp_cons(pec12[[2]], bal12), "Not a matrix")
   expect_error(sp_cons(pec12[[2]], list(BalancedTree(12))),
                "not a RawMatrix")
@@ -28,6 +29,8 @@ test_that("Split contradiction is calculated", {
                !c(rep(TRUE, 3), FALSE, FALSE, rep(TRUE, 4)))
   expect_equal(SplitConsistent(pec12[[5]], list(bal12, bal12))[[2]],
                rep(TRUE, length(bal12)))
-  expect_equal(SplitConsistent(pec12[[5]], list(as.Splits(StarTree(12))))[[1]],
+  expect_equal(SplitConflicts(pec12[[5]], as.Splits(StarTree(12)))[[1]],
                logical(0))
+  expect_equal(SplitConflicts(pec12[[2]], list(bal12, pec12)),
+               lapply(SplitConsistent(pec12[[2]], list(bal12, pec12)), `!`))
 })
