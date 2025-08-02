1c9378a61653c95ecc440437aae84ef44e0b5e62
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-08-02 15:10:57 +0100

    Fix benchmark

--- benchmark/bench-SplitList.R
+++ benchmark/bench-SplitList.R
@@ -6,10 +6,10 @@ someTrees <- as.phylo(1:240, 64*3)
 Benchmark("as.Splits192", ub(as.Splits(someTrees), times = 100)) # 16.5
 Benchmark("as.Splits64.32", ub(as.Splits(bigTrees), times = 100)) # 23.4
 
-bigSplits <- as.Splits(bigTree)
-someSplits <- as.Splits(someTree)
+bigSplits <- as.Splits(bigTrees)
+someSplits <- as.Splits(someTrees)
 
 # Test: -3 or keep power of 2 for SL_MAX_SPLITS?
 # See: #define SL_MAX_SPLITS (SL_MAX_TIPS - 3) in SplitList.h
-Benchmark("as.phylo.some", ub(as.phylo(someSplits), times = 1000))
-Benchmark("as.phylo.big", ub(as.phylo(bigSplits), times = 100))
+Benchmark("as.phylo.some", ub(lapply(someSplits, as.phylo), times = 100)) # 16.7
+Benchmark("as.phylo.big", ub(lapply(bigSplits, as.phylo), times = 100)) # 34.0
