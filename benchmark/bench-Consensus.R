c012600c5ae4ca089151766d899374901d0a02fb
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-08-02 23:32:56 +0100

    Comment out RF benchmarks
    TreeDist will segfault

--- benchmark/bench-Consensus.R
+++ benchmark/bench-Consensus.R
@@ -1,10 +1,10 @@
 source("benchmark/_init.R")
-library("TreeDist")
+# library("TreeDist")
 
 forest1 <- as.phylo(0:200, 80)
 forest2 <- as.phylo(0:20, 260)
 
 Benchmark("consensus1", ub(Consensus(forest1), times = 250))
 Benchmark("consensus2", ub(Consensus(forest2), times = 500))
-Benchmark("RF1", ub(RobinsonFoulds(forest1), times = 42))
-Benchmark("RF2", ub(RobinsonFoulds(forest2), times = 250))
+# Benchmark("RF1", ub(RobinsonFoulds(forest1), times = 42))
+# Benchmark("RF2", ub(RobinsonFoulds(forest2), times = 250))
