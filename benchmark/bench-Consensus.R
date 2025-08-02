1233d54cfa7bf37817e6c6e8321c69dd18020405
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-08-02 15:46:27 +0100

    Pause until TreeDist install is stable

--- benchmark/bench-Consensus.R
+++ benchmark/bench-Consensus.R
@@ -5,5 +5,6 @@ forest2 <- as.phylo(0:20, 260)
 
 Benchmark("consensus1", ub(Consensus(forest1)))
 Benchmark("consensus2", ub(Consensus(forest2)))
-Benchmark("RF1", ub(RobinsonFoulds(forest1), times = 42))
-Benchmark("RF2", ub(RobinsonFoulds(forest2)))
+
+# Benchmark("RF1", ub(RobinsonFoulds(forest1), times = 42))
+# Benchmark("RF2", ub(RobinsonFoulds(forest2)))
