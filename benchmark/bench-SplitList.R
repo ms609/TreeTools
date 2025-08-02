10ee8f026df16522d09b27f02d7278018e02ba88
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-08-02 14:59:36 +0100

    Remember to init, innit

--- benchmark/bench-SplitList.R
+++ benchmark/bench-SplitList.R
@@ -3,8 +3,8 @@ source("benchmark/_init.R")
 bigTrees <- as.phylo(1:10, 64*32)
 someTrees <- as.phylo(1:240, 64*3)
 
-Benchmark("as.Splits192", ub(as.Splits(someTrees), times = 100))
-Benchmark("as.Splits64.32", ub(as.Splits(bigTrees), times = 100))
+Benchmark("as.Splits192", ub(as.Splits(someTrees), times = 100)) # 16.5
+Benchmark("as.Splits64.32", ub(as.Splits(bigTrees), times = 100)) # 23.4
 
 bigSplits <- as.Splits(bigTree)
 someSplits <- as.Splits(someTree)
