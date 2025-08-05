bb831e7346cd5cc1c8b2956e7750953f12f0d1f9
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-08-05 19:07:19 +0100

    shQuote

--- benchmark/_run_benchmarks.R
+++ benchmark/_run_benchmarks.R
@@ -1,4 +1,4 @@
 benchFiles <- list.files("benchmark", "^bench\\-.*\\.R$", full.names = TRUE)
 for (benchFile in benchFiles) {
-  system2("Rscript", c("-e", paste0("source('", benchFile, "')")))
+  system2("Rscript", c("-e", shQuote(paste0("source('", benchFile, "')"))))
 }
