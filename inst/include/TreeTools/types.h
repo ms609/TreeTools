7c3a13c60c1ff7a1b4658dcb9e108f9db472ee68
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2026-01-05 14:07:36 +0000

    numeric_limits()

--- inst/include/TreeTools/types.h
+++ inst/include/TreeTools/types.h
@@ -2,6 +2,7 @@
 #define TreeTools_types_
 
 #include <cstdint> /* for uint_fast32_t, int_fast32_t, INTPTR_MAX */
+#include <limits> /* for numeric_limits */
 
 using int16 = int_fast16_t;
 using int32 = int_fast32_t;
@@ -9,8 +10,8 @@ using intx = int_fast32_t;
 using uintx = uint_fast32_t;
 
 namespace TreeTools {
-  const intx INTX_MAX = intx(INT_FAST32_MAX);
-  const uintx UINTX_MAX = uintx(UINT_FAST32_MAX);
+  const intx INTX_MAX = intx(std::numeric_limits<intx>::max());
+  const uintx UINTX_MAX = uintx(std::numeric_limits<uintx>::max());
 }
 
 #endif
